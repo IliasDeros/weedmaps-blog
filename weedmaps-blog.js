27fed1135ac829f27beacf887a47a9e536b026b9
Ilias Deros <ideros21@gmail.com>
2016-02-02 23:16:49 +0000

    Add post update

--- weedmaps-blog.js
+++ weedmaps-blog.js
@@ -1,6 +1,7 @@
 var Posts = new Mongo.Collection('posts');
 var writingPost = false;
-var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 
+          'July', 'August', 'September', 'October', 'November', 'December']
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
@@ -11,17 +12,28 @@ if (Meteor.isServer) {
 if (Meteor.isClient) {
   Template.body.helpers({
     posts: getPosts,
-    writingPost: function(){ return Session.get('writingPost'); }
+    writingPost: function(){ return Session.get('writingPost'); },
   });
   
   Template.body.events({
     'click #new-post': toggleNewPost,
-    'submit .new-post': submitPost,
-    'click .delete': deletePost
+    'submit .new-post': submitPost
+  });
+  
+  Template.post.helpers({
+    editingPost: function(){ return Session.get('editingPost') == this._id; }
+  });
+  
+  Template.post.events({
+    'click .delete': deletePost,
+    'click .edit': editPost,
+    'click .edit-post-cancel': cancelEdit,
+    'submit .edit-post': updatePost
   });
   
   Template.registerHelper('formatDate', function(date){
-    return monthNames[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
+    return monthNames[date.getMonth()] 
+      + " " + date.getDate() + ", " + date.getFullYear();
   });
   
   Accounts.ui.config({
@@ -34,6 +46,35 @@ if (Meteor.isClient) {
 // toggle new post creation mode
 function toggleNewPost(){
   Session.set('writingPost', writingPost = !writingPost);
+}
+
+// leave post edit mode
+function cancelEdit(e){
+  e.preventDefault();
+  
+  Session.set('editingPost', null);
+}
+
+// update a post if fields are valid
+function updatePost(e){
+  e.preventDefault();
+  
+  var newTitle = e.target.title.value;
+  var newDescription = e.target.description.value;
+  
+  // post fields cannot be empty
+  if (!newTitle.length || !newDescription.length){
+    return false;
+  }
+  
+  Posts.update(this._id, {
+    $set: {
+      title: newTitle,
+      description: newDescription
+    }
+  });
+  
+  Session.set('editingPost', null);
 }
 
 // submit a new post
@@ -60,8 +101,14 @@ function submitPost(e){
   Session.set('writingPost', false);
 }
 
+// delete a post
 function deletePost(e){
   Posts.remove(this._id);
+}
+
+// toggle post edit mode
+function editPost(){
+  Session.set('editingPost', this._id);
 }
 
 // get a list of all posts
@@ -69,6 +116,7 @@ function getPosts(){
   var posts = [];
   var currentUserId = Meteor.userId();
   
+  // display only user's posts if currently logged in
   if (currentUserId){
     posts = Posts.find({owner: currentUserId}, {sort: {creationDate: -1}});
   } else {
