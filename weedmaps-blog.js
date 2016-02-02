c5241088375f0525731d64da8ae430ca10184a76
Ilias Deros <ideros21@gmail.com>
2016-02-02 21:01:42 +0000

    add basic post Create, Read and delete

--- weedmaps-blog.js
+++ weedmaps-blog.js
@@ -1,9 +1,57 @@
+var Posts = new Mongo.Collection('posts');
+var writingPost = false;
+
 if (Meteor.isClient) {
-  // code to run on client
+  Template.body.helpers({
+    posts: getPosts,
+    writingPost: function(){ return Session.get('writingPost'); }
+  });
+  
+  Template.body.events({
+    'click #new-post': toggleNewPost,
+    'submit .new-post': submitPost,
+    'click .delete': deletePost
+  })
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+}
+
+// toggle new post creation mode
+function toggleNewPost(){
+  Session.set('writingPost', writingPost = !writingPost);
+}
+
+// submit a new post
+function submitPost(e){
+  e.preventDefault();
+  var title = e.target.title.value;
+  var description = e.target.description.value;
+  
+  // form must be complete
+  if (!title.length || !description.length){
+    return false;
+  }
+  
+  Posts.insert({
+    title: title,
+    description: description,
+    creationDate: new Date()
+  });
+  
+  // clear posts writing form
+  e.target.reset();
+  Session.set('writingPost', false);
+}
+
+function deletePost(e){
+  Posts.remove(this._id);
+}
+
+// get a list of all posts
+function getPosts(){
+  return Posts.find({}, {sort: {creationDate: -1}});
 }
